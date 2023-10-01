ce4f02714cb90c925d86047f779da20415d94b34
ConradBelaire <cbelaire@gmail.com>
2023-10-01 19:16:19 -0400

    Functionality, game over returns to menu after timer

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -2,9 +2,6 @@ extends Node2D
 
 var lives = 3
 
-func _update_lives():
-	print(get_node("/root/").get_child(1).find_child("UI").name)
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
